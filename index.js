e3d88528c3f9b197e8d7b5ab1fe3c17b380e7e8c
Robert Williams <robwillcode@gmail.com>
2015-05-03 12:24:44 -0700

    refactoring in process

--- index.js
+++ index.js
@@ -1,81 +1,36 @@
 var secure = require('./lib/secure.js');
 var survey = require('./lib/survey.js');
-var database = require('./lib/database.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var pg = require('pg');
 
 var jsonParser = bodyParser.json();
 
 app.set('port', (process.env.PORT || 5000));
 
-//400 bad request
-//401 unauthorized access
-//497 HTTP to HTTPS
-
 //Request Image Storage Key
 app.get('/image_auth', function(req, res){
-	//Check to see if connection is using SSL
-	if(secure.connection(req)){
-		survey.imageAuth(req, res);
-	}
-	else{
-		res.type('text/plain');
-		res.status(497);
-		res.send('497 - HTTP to HTTPS');
-	}
+	secure.connection(req, res, survey.imageAuth);
 });
 
+//Request list of surveys in interim database
 app.get('/getSurveys', function(req, res){
-	//Check to see if connection is using SSL
-	if(secure.connection(req)){
-		survey.getSurveys(req, res);
-	}
-	else{
-		res.type('text/plain');
-		res.status(497);
-		res.send('497 - HTTP to HTTPS');
-	}
+	secure.connection(req, res, survey.getSurveys);
 });
 
 //Request a Survey
 app.get('/download', function(req, res){
-	//Check to see if connection is using SSL
-	if(secure.connection(req)){
-		survey.download(req, res);
-	}
-	else{
-		res.type('text/plain');
-		res.status(497);
-		res.send('497 - HTTP to HTTPS');
-	}
+	secure.connection(req, res, survey.download);
 });
 
 //Upload a Survey
 app.post('/upload', jsonParser, function(req, res){
-	if(secure.connection(req)){
-		survey.upload(req, res);
-	}
-	else{
-		res.type('text/plain');
-		res.status(497);
-		res.send('497 - HTTP to HTTPS');
-	}
+	secure.connection(req, res, survey.upload);
 });
 
 //404 - Not Found
 app.use(function(req, res){
-	res.type('text/plain');
-	if(secure.connection(req)){
-		res.status(404);
-		res.send('404 - Not Found');
-	}
-	else{
-		res.status(497);
-		res.send('497 - HTTP to HTTPS');
-	}
-
+	secure.connection(req, res, survey.missingPage);
 });
 
 //500 - Internal Server Error

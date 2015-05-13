0cd51ef4a433645188fb67c72d01ca5d87c4ca27
Neil McMunn <neil.mcmunn@gmail.com>
2015-05-13 12:05:57 -0700

    Added jsonParser middleware to upload route

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var secure = require('./lib/secure.js');
 var survey = require('./lib/survey.js');
 var express = require('express');
-var connectTimeout = require('connect-timeout');
+var bodyParser = require('body-parser');
+//var connectTimeout = require('connect-timeout');
+
 var app = express();
-var timout = connectTimeout({time:5000});
+var jsonParser = bodyParser.json();
+//var timout = connectTimeout({time:5000});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -28,7 +31,7 @@ app.post('/upload', function(req, res){
 });
 
 //Upload a Survey
-app.post('/upload', function(req, res){
+app.post('/upload', jsonParser, function(req, res){
 	secure.connection(req, res, survey.upload);
 });
 
